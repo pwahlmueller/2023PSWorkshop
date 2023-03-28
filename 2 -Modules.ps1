4adf683fef12642a167ad829888c66bf16e38c4e
Patrick Wahlmueller <patrick@wahlmueller.it>
2023-03-28 22:48:36 +0200

    Changes

--- 2 -Modules.ps1
+++ 2 -Modules.ps1
@@ -4,17 +4,21 @@
 return 'You should not pass!'
 #region Step 1 - Find Module
 
-[system.Diagnostics.Process]::Start("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "https://www.powershellgallery.com/packages?q=sql")
+[system.Diagnostics.Process]::Start("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://www.powershellgallery.com/packages?q=sql")
 
 #endregion  Step 1 - Find Module
 
 #region Step 2 - Get the Module
-Install-Module -Name dbatools -Force
-Install-Module -Name sqlserver -force -AllowClobber
 
-(get-command -module dbatools).count
-(get-command -module sqlserver).count
+get-module
+get-module -ListAvailable | out-gridview
 
-import-module dbatools -force
-Import-Module sqlserver
+install-module -Name PowerShellAI
+
+get-command -Module PowerShellAI
+
+get-module PowerShellAI | select ModuleBase
+
+. explorer.exe (get-module PowerShellAI ).ModuleBase
+
 #endregion Step 2
